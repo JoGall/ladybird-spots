8798c1d88a9d0f4162aa18c51c513d7183a935b8
jogall <joedgallagher@gmail.com>
2016-10-13 18:43:31 +0100

    implemented adaptive threshold, split red channel for spot detection

--- spots.cpp
+++ spots.cpp
@@ -1,6 +1,9 @@
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <highgui.h>
-// #include <std_lib_facilities.h>
+#include <string>
+// #include <boost/filesystem.hpp>
+
 using namespace cv;
 using namespace std;
 
@@ -16,118 +19,139 @@ int countDark(Mat input, Mat mask, int threshold){
         }
 	return nPixels;
 }
-
+  
 ///MAIN
 int main( int argc, char** argv ) {
 	
-	//STORAGE
-	Mat img_src, img_grey, img_blur, img_thresh, img_keypoints, img_spots, img_output2;
-	vector<KeyPoint> keypoints;
-	vector<vector<Point> > contours;
-	
-	//READ IMAGE AND CONVERT
-	img_src = imread(argv[2], 1);
-	cvtColor(img_src, img_grey, CV_BGR2GRAY);
-	
-	//PRINT RESULTS HEADER
-	cout << "filename" <<"\t"<< "elytron_id" <<"\t"<< "n_spots" <<"\t"<< "spot_px" <<"\t"<< "elytron_px" <<"\t"<< "elytron_H" <<"\t"<< "elytron_W" << endl;
-		
-	///FIND ALL ELYTRA
-	//PROCESS IMAGE
-	blur(img_grey, img_blur, Size(9,9));
-	threshold(img_blur, img_thresh, 155, 255, CV_THRESH_BINARY_INV);
-// 	bilateralFilter(img_grey, img_blur, 25, 50,50);
-// 	adaptiveThreshold(img_grey, img_thresh, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 201,10);
-
-	//FIND ELYTRA CONTOURS
-	findContours(img_thresh.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-	//sort contours by size
-	vector<int> contourIndices(contours.size());
-	iota(contourIndices.begin(), contourIndices.end(), 0);
-	sort(contourIndices.begin(), contourIndices.end(), [&contours](int lhs, int rhs) {
-        return contours[lhs].size() > contours[rhs].size();
-    });
-	//set number of elytra to use
-    int n_elytra = strtol(argv[1], NULL, 10);
-    n_elytra = min(n_elytra, int(contours.size()));
-
-	
-	///ELYTRA LOOP
-    for (int i = 0; i < n_elytra; ++i) {
-		
-		//DRAW CONTOUR AND MASK ORIGINAL IMAGE
-		Mat img_contour(img_grey.size(), CV_8U, Scalar(0));
-		drawContours(img_contour, contours, contourIndices[i], Scalar(255), CV_FILLED);
-		Mat img_mask(img_grey.size(), CV_8U, Scalar(255));
-		img_grey.copyTo(img_mask, img_contour);
-		
-		///COUNT SPOT PIXELS AND ELYTRON PIXELS
-		int mask_pixels = cv::countNonZero(img_contour);
-		int spot_pixels = countDark(img_grey, img_contour, 80);
-		
-		///COUNT N SPOTS
-		//INITIALISE VALUES
-		double minThresh_slider = 0, maxThresh_slider = 96, minArea_slider = 1000, maxArea_slider = 8000, minDistBetweenBlobs_slider = 10, minRepeatability_slider = 1, minCircularity_slider = 0.2, maxCircularity_slider = 1, minConvexity_slider = 0.65, maxConvexity_slider = 1, minInertiaRatio_slider = 0.1, maxInertiaRatio_slider = 1;
-			
-		//DEFINE PARAMETERS
-		SimpleBlobDetector::Params params;
-		params.filterByArea = true;
-		params.minArea = minArea_slider;
-		params.maxArea = maxArea_slider;
-		params.minDistBetweenBlobs = minDistBetweenBlobs_slider;
-		params.minRepeatability = minRepeatability_slider;
-		params.filterByColor = true;
-		params.minThreshold = minThresh_slider;
-		params.maxThreshold = maxThresh_slider;
-		params.filterByCircularity = true;
-		params.minCircularity = minCircularity_slider;
-		params.maxCircularity = maxCircularity_slider;
-		params.filterByConvexity = true;
-		params.minConvexity = minConvexity_slider;
-		params.maxConvexity = maxConvexity_slider;
-		params.filterByInertia = true;
-		params.minInertiaRatio = minInertiaRatio_slider;
-		params.maxInertiaRatio = maxInertiaRatio_slider;
-		
-		//DETECT
-		SimpleBlobDetector detector(params);
-		detector.detect( img_mask, keypoints);
-
-		///CREATE OUTPUT IMAGE
-		//draw elytron contour (in grey against white background)
-		Mat img_output(img_grey.size(), CV_8U, Scalar(0));
-		drawContours(img_output, contours, contourIndices[i], Scalar(200), CV_FILLED);
-		bitwise_not(img_output, img_output);
-		//add spots in black
-		threshold(img_mask, img_spots, 80, 255, CV_THRESH_BINARY);
-		addWeighted(img_output, 0.2, img_spots, 0.8, 0, img_output);
-		//draw blob locations
-		drawKeypoints( img_output, keypoints, img_output, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-		//draw minimum enclosing rectangle to elytron contour
-		RotatedRect minRect = minAreaRect(Mat(contours[contourIndices[i]]));
-		Point2f vertices[4];
-		minRect.points(vertices);
-		for (int j = 0; j < 4; j++) {
-			line(img_output, vertices[j], vertices[(j+1)%4], Scalar(0,0,255),5);
-		}
-		//find rightmost point of elytron contour
-		Point extRight = *max_element(contours[contourIndices[i]].begin(), contours[contourIndices[i]].end(), [](const Point& lhs, const Point& rhs) {
-			return lhs.x < rhs.x;
-        });
-		//add text for elytron id
-		string elytron_id = to_string(i);
-		putText(img_output, elytron_id, extRight, FONT_HERSHEY_SIMPLEX, 8, Scalar(0,0,0), 40, 4);
-		
-		///OUTPUTS
-		//PRINT RESULTS
-		cout << argv[2] <<"\t"<< i <<"\t"<< keypoints.size() <<"\t"<< spot_pixels <<"\t"<< mask_pixels <<"\t"<< minRect.size.height <<"\t"<< minRect.size.width << endl;
-		
-		//WRITE IMAGE MASK
-		ostringstream text;
-		text << "mask_" << i << ".jpg";
-		imwrite(text.str(), img_output);
+	//error handling
+	if (argc<3) {
+		std::cout << "Error!\nUsage is: " <<argv[0]<< " n_elytra file_1 file_2..." << std::endl;
+		return 0;
 	}
 	
-	return 1;
+	//print results header
+		cout << "filename" <<"\t"<< "elytron_id" <<"\t"<< "n_spots" <<"\t"<< "spot_px" <<"\t"<< "elytron_px" <<"\t"<< "elytron_H" <<"\t"<< "elytron_W" << endl;
 	
+	//FILELOOP
+	for(int i=2; i<argc; i++){
+		
+		//STORAGE
+		Mat img_src, img_grey, img_hsv, img_blur, img_thresh, img_keypoints, img_spots;
+		vector<KeyPoint> keypoints;
+		vector<vector<Point> > contours;
+		
+		//READ IMAGE AND CONVERT COLOURSPACE
+		img_src = imread(argv[i], CV_RGB2BGR);
+		cvtColor(img_src, img_grey, CV_BGR2GRAY);
+		Mat img_bgr[3];
+		split(img_src, img_bgr);
+			
+		///FIND ALL ELYTRA
+		//PROCESS IMAGE
+		GaussianBlur(img_bgr[0], img_blur, Size(99,99), 0);
+// 		blur(img_grey, img_blur, Size(9,9));
+		adaptiveThreshold(img_blur, img_thresh, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY_INV, 99, 6);
+		//dilate and erode
+// 		dilate(img_thresh, img_thresh, Mat(Size(15,15), CV_8U, Scalar(255)));
+// 		erode(img_thresh, img_thresh, Mat(Size(15,15), CV_8U, Scalar(255)));
+
+		//FIND ELYTRA CONTOURS
+		findContours(img_thresh.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+		//sort contours by size
+		vector<int> contourIndices(contours.size());
+		iota(contourIndices.begin(), contourIndices.end(), 0);
+		sort(contourIndices.begin(), contourIndices.end(), [&contours](int lhs, int rhs) {
+			return contours[lhs].size() > contours[rhs].size();
+		});
+		//set number of elytra to use
+		int n_elytra = strtol(argv[1], NULL, 10);
+		n_elytra = min(n_elytra, int(contours.size()));
+
+		
+		///ELYTRA LOOP
+		for (int j = 0; j < n_elytra; ++j) {
+			
+			//draw elytron contour and mask original image
+			Mat img_contour(img_grey.size(), CV_8U, Scalar(0));
+			Mat img_mask(img_grey.size(), CV_8U, Scalar(255));
+// 			Mat img_src_mask(img_src.size(), CV_32F, Scalar(255));
+			
+			drawContours(img_contour, contours, contourIndices[j], Scalar(255), CV_FILLED);
+			img_bgr[2].copyTo(img_mask, img_contour);
+// 			img_src.copyTo(img_src_mask, img_contour);
+			
+			//find spot contours
+			Mat mask_blur, mask_thresh;
+			GaussianBlur(img_mask, mask_blur, Size(49,49), 0);
+			adaptiveThreshold(mask_blur, mask_thresh, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 299, 25);
+// 			threshold(img_mask, mask_thresh, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+// 			adaptiveThreshold(img_mask, mask_thresh, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 11, 2);
+			
+			///COUNT SPOT PIXELS AND ELYTRON PIXELS
+			int mask_pixels = cv::countNonZero(img_contour);
+			int spot_pixels = countDark(img_grey, img_contour, 80);
+			
+			///COUNT N SPOTS WITH BLOB DETECTION
+			//define parameters
+			SimpleBlobDetector::Params params;
+			params.filterByArea = true;
+			params.minArea = 2000;
+			params.maxArea = 80000;
+			params.minDistBetweenBlobs = 10;
+			params.minRepeatability = 1;
+// 			params.filterByColor = true;
+// 			params.minThreshold = minThresh_slider;
+// 			params.maxThreshold = maxThresh_slider;
+			params.filterByCircularity = true;
+			params.minCircularity = 0.2;
+			params.maxCircularity = 1;
+			params.filterByConvexity = true;
+			params.minConvexity = 0.65;
+			params.maxConvexity = 1;
+			params.filterByInertia = true;
+			params.minInertiaRatio = 0.1;
+			params.maxInertiaRatio = 1;
+			
+			//detect
+			SimpleBlobDetector detector(params);
+			detector.detect( img_mask, keypoints);
+
+			///CREATE OUTPUT IMAGE
+			//draw elytron contour (in grey against white background)
+			Mat img_output(img_grey.size(), CV_8U, Scalar(0));
+			drawContours(img_output, contours, contourIndices[j], Scalar(200), CV_FILLED);
+			bitwise_not(img_output, img_output);
+			//add spots in black
+			threshold(img_mask, img_spots, 80, 255, CV_THRESH_BINARY);
+			addWeighted(img_output, 0.2, img_spots, 0.8, 0, img_output);
+			//draw blob locations
+			drawKeypoints( img_output, keypoints, img_output, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+			//draw minimum enclosing rectangle to elytron contour
+			RotatedRect minRect = minAreaRect(Mat(contours[contourIndices[j]]));
+			Point2f vertices[4];
+			minRect.points(vertices);
+			for (int k = 0; k < 4; k++) {
+				line(img_output, vertices[k], vertices[(k+1)%4], Scalar(0,0,255),5);
+			}
+			//find rightmost point of elytron contour
+			Point extRight = *max_element(contours[contourIndices[j]].begin(), contours[contourIndices[j]].end(), [](const Point& lhs, const Point& rhs) {
+				return lhs.x < rhs.x;
+			});
+			//add text for elytron id
+			string elytron_id = to_string(j);
+			putText(img_output, elytron_id, extRight, FONT_HERSHEY_SIMPLEX, 8, Scalar(0,0,0), 40, 4);
+			//make image name
+			string FileName = argv[i];
+			FileName.erase(FileName.find(".jpg"));
+			std::ostringstream text;
+			text << FileName << "_mask" << j << ".jpg";
+			//write image
+			imwrite(text.str(), img_output);
+			
+			///OUTPUT DATA
+			cout << FileName <<"\t"<< j <<"\t"<< keypoints.size() <<"\t"<< spot_pixels <<"\t"<< mask_pixels <<"\t"<< minRect.size.height <<"\t"<< minRect.size.width << endl;
+		
+		}
+	}
+	return 1;
 }
